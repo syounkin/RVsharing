78fc1f297b760dbac696a737262eb9218f045d95
Samuel Younkin <syounkin@jhsph.edu>
2013-05-20 08:22:08 -0400

    Created zzz.R
    This file is executed when the package is loaded.  Currently it prints a welcome meassage and nothing else.

---
+++ R/zzz.R
@@ -0,0 +1,10 @@
+THISPKG <- "Bureau"
+.onAttach <- function(libname, pkgname) {
+	version <- packageDescription("Bureau", field="Version")
+	packageStartupMessage(paste("
+Welcome to Bureau version ", version, "\n", sep = "" ) )
+}
+
+.onUnload <- function(libpath){
+	library.dynam.unload(THISPKG, libpath)
+}
