c68ed09d2ebf49b40bf6ea772d6408ae71e7ef92
Samuel Younkin <syounkin@jhsph.edu>
2013-05-20 16:52:35 -0400

    Updated RVsharing vignette

--- vignette/RVsharing.R
+++ vignette/RVsharing.R
@@ -8,12 +8,12 @@
 
 
 ###################################################
-### code chunk number 2: RVsharing.toy
+### code chunk number 2: RVsharing.toy2
 ###################################################
-id <- paste0("sub", 1:8)
-fa.id <- c(NA,NA,NA,"sub1","sub1", NA,"sub3","sub5")
-ma.id <- c(NA,NA,NA,"sub2","sub2", NA,"sub4","sub6")
-test.ped <- pedigree(id = id, dadid = fa.id, momid = ma.id, sex = c(1,2,1,2,1,2,2,2))
+id <- paste0("sub", 1:4)
+fa.id <- c(NA,NA,"sub1","sub1")
+ma.id <- c(NA,NA,"sub2","sub2")
+test.ped <- pedigree(id = id, dadid = fa.id, momid = ma.id, sex = c(1,2,1,2))
 
 
 ###################################################
@@ -29,7 +29,46 @@ plot(test.ped)
 
 
 ###################################################
-### code chunk number 5: test
+### code chunk number 5: kinship
+###################################################
+2*kinship(test.ped)
+
+
+###################################################
+### code chunk number 6: test
+###################################################
+RVsharing(test.ped)
+
+
+###################################################
+### code chunk number 7: RVsharing.toy
+###################################################
+id <- paste0("sub", 1:8)
+fa.id <- c(NA,NA,NA,"sub1","sub1", NA,"sub3","sub5")
+ma.id <- c(NA,NA,NA,"sub2","sub2", NA,"sub4","sub6")
+test.ped <- pedigree(id = id, dadid = fa.id, momid = ma.id, sex = c(1,2,1,2,1,2,2,2))
+
+
+###################################################
+### code chunk number 8: showped
+###################################################
+test.ped
+
+
+###################################################
+### code chunk number 9: plotped2
+###################################################
+plot(test.ped)
+
+
+###################################################
+### code chunk number 10: kinship
+###################################################
+2*kinship(test.ped)
+
+
+###################################################
+### code chunk number 11: test
 ###################################################
 RVsharing(test.ped)
 
