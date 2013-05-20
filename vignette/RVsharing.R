c7613e600b982171b5b53e797bb223694e56793e
Samuel Younkin <syounkin@jhsph.edu>
2013-05-20 16:23:30 -0400

    Updated RVsharing vignette
    Now considers a toy example with two first cousins.

--- vignette/RVsharing.R
+++ vignette/RVsharing.R
@@ -13,6 +13,24 @@
 id <- paste0("sub", 1:8)
 fa.id <- c(NA,NA,NA,"sub1","sub1", NA,"sub3","sub5")
 ma.id <- c(NA,NA,NA,"sub2","sub2", NA,"sub4","sub6")
-Bureau:::RVsharing( id = id, dad.id = fa.id, mom.id = ma.id )
+test.ped <- pedigree(id = id, dadid = fa.id, momid = ma.id, sex = c(1,2,1,2,1,2,2,2))
+
+
+###################################################
+### code chunk number 3: showped
+###################################################
+test.ped
+
+
+###################################################
+### code chunk number 4: plotped
+###################################################
+plot(test.ped)
+
+
+###################################################
+### code chunk number 5: test
+###################################################
+RVsharing(test.ped)
 
 
