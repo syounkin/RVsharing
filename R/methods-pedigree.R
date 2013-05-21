d42e0d5ce4e9d36cba4e811ade9b29966add097b
Samuel Younkin <syounkin@jhsph.edu>
2013-05-21 17:37:26 -0400

    Removed commented-out code.

--- R/methods-pedigree.R
+++ R/methods-pedigree.R
@@ -1,39 +1,3 @@
-## setMethod("initialize", signature(.Object="PedClass"),
-##          function(.Object, ...){
-##            .Object
-##            callNextMethod(.Object, ... )
-##          })
-
-## setMethod( "PedClass", signature(object = "DataFrame"), function(object) {
-##   ped.DF <- with( as(object, "data.frame"), {
-##     DataFrame( famid = factor(famid),
-##               id = factor(id),
-##               fid = factor(fid),
-##               mid = factor(mid),
-##               sex = factor(sex),
-##               dx = factor(dx) )
-##   })
-##   new("PedClass", ped.DF)
-## })
-
-## setMethod("trios",  signature(object="PedClass"), function(object) {
-##   trio.df <- subset( as(object, "data.frame" ), !is.na(fid) & !is.na(mid) & !is.na(id) )
-## #  id.char <- as.character(trio.df$id)#,
-##   trio.df.2 <- data.frame( id = as.character(trio.df$id),
-##                           fid = as.character(trio.df$fid),
-##                           mid = as.character(trio.df$mid), stringsAsFactors=FALSE )
-## #                          )
-##   return(trio.df.2)
-## })
-
 setMethod("RVsharing",  signature(object="pedigree"), function(object){
     RVsharing.fn( id = object$id, dad.id = ifelse(object$findex!=0,object$id[object$findex],0), mom.id = ifelse(object$mindex!=0,object$id[object$mindex],0) )
 })
-
-## setMethod("offspring",  signature(object="PedClass"), function(object){
-##     with(as(object,"data.frame"), unique(as.character(id)))
-## })
-
-## setMethod("allSubjects",  signature(object="PedClass"), function(object){
-##     with(as(object,"data.frame"), unique(c(as.character(id), as.character(fid), as.character(mid))))
-## })
