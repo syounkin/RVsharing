e1f09fbac5beff5635646ba85f2c1ed406907cb4
Samuel Younkin <syounkin@jhsph.edu>
2013-06-20 18:10:47 -0400

    Updated RVsharing vignette.
    The method RVsharing works for both classes for siblings and first cousins.  Neither class works for second cousin.
    
    It is not clear to me what the missing data code is for RVsharing.  It seems that NA is working, but the code makes it look like it might be a zero.

--- R/methods-pedigree.R
+++ R/methods-pedigree.R
@@ -1,5 +1,11 @@
 setMethod("RVsharing",  signature(ped="pedigree", id = "missing",dad.id = "missing",mom.id = "missing"), function(ped){
-    RVsharing.fn( id = ped$id, dad.id = ifelse(ped$findex!=0,ped$id[ped$findex],0), mom.id = ifelse(ped$mindex!=0,ped$id[ped$mindex],0) )
+
+  fa.index <- ifelse(ped$findex !=0, ped$findex, NA)
+  fa.id <- ped$id[fa.index]
+  ma.index <- ifelse(ped$mindex !=0, ped$mindex, NA)
+  ma.id <- ped$id[ma.index]
+  
+    RVsharing.fn( id = ped$id, dad.id = fa.id, mom.id = ma.id )
 })
 setMethod("RVsharing",  signature(ped = "missing", id="character",dad.id="character",mom.id="character"), function(id, dad.id, mom.id){
     RVsharing.fn( id = id, dad.id = dad.id, mom.id = mom.id )
