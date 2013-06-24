e1cb2c72dc8683f2172b8e3e57a539c0f3f62c52
Samuel Younkin <syounkin@jhsph.edu>
2013-06-24 11:29:37 -0400

    Trying to drop through inbred pedigrees.
    I am still having some difficulties dropping through pedigrees with inbreeding.

--- R/methods-trio.R
+++ R/methods-trio.R
@@ -7,17 +7,24 @@ setMethod("initialize", "Trio", function(.Object, ... ){
 setMethod("GeneDrop", signature( trio = "Trio", geno.vec = "numeric"), function( trio, geno.vec ){
   g1 <- geno.vec[trio@id]
   g2 <- geno.vec[trio@spouse]
-  if( is.na(g1) | is.na(g2) ) stop
-  for (i in 1:length(trio@offspring)){
+#  if( is.na(g1) | is.na(g2) ) stop
+#  while( any(is.na(geno.vec)) ){
+  for (i in sample(1:length(trio@offspring), replace = FALSE)){
     if( is.character(trio@offspring[[i]]) ){
       goff <- gene.drop.fn(g1,g2)
-      geno.vec[ trio@offspring[[i]] ] <- goff
+      if( is.na(geno.vec[ trio@offspring[[i]]])){
+        geno.vec[ trio@offspring[[i]] ] <- goff
+      }
     }else{
       # this is the case where the offspring is a trio object
       goff <- gene.drop.fn(g1,g2)
-      geno.vec[ trio@offspring[[i]]@id ] <- goff
-      geno.vec <- GeneDrop(trio@offspring[[i]], geno.vec)
+      if( is.na(geno.vec[ trio@offspring[[i]]@id])){
+        geno.vec[ trio@offspring[[i]]@id ] <- goff
+        geno.vec <- GeneDrop(trio@offspring[[i]], geno.vec)
+      }
     }
   }
+  cat(geno.vec, "\n")
+#}
   return( geno.vec )
 })
