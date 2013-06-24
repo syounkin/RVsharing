783b5545ccba9b404c6a2903d8171556dc3b2904
Samuel Younkin <syounkin@jhsph.edu>
2013-06-24 15:05:34 -0400

    Gene Dropping for Inbred Pedigrees.
    It seems to work now for inbred second cousins.  See http://www.biostat.jhsph.edu/~syounkin/vignettes/RVsharing.html#_inbreeding

--- R/functions.R
+++ R/functions.R
@@ -1,5 +1,5 @@
 gene.drop.fn <- function(g1,g2){
-  if( !is.na(g1) & is.na(g2) ){
+  if( is.na(g1) | is.na(g2) ){
     goff <- NA
   }else if( g1 == 0 && g2 == 1 ){
     goff <- sample( x = 0:1, size = 1, prob = rep(0.5,2) )
