9c36a2c4c8812f210d87ce32b6ec3ad216f4a6ca
Samuel Younkin <syounkin@jhsph.edu>
2013-08-05 10:23:07 -0400

    Updated ped2trio vignette.

--- vignettes/ped2trio.asciidoc.R
+++ vignettes/ped2trio.asciidoc.R
@@ -10,7 +10,7 @@ par(mar=rep(0,4))
 ### code chunk number 2: options
 ###################################################
   options(width=75, continue = " ")
-  library("Bureau")
+  library("RVsharing")
 
 
 ###################################################
@@ -27,31 +27,36 @@ test.ped.first.cousins <- pedigree(id = id, dadid = fa.id, momid = ma.id, sex = 
 ###################################################
 plot(test.ped.first.cousins)
 
+
 ###################################################
-### code chunk number 5: ped2trio
+### code chunk number 5: firstcousins
 ###################################################
+
 trio.obj <- RVsharing:::ped2trio(test.ped.first.cousins)
 
 geno.vec <- c(1,0,0,NA,NA,0,NA,NA)
 names(geno.vec) <- paste0("sub",1:8)
 
-RVsharing:::GeneDropSim.fn( trio = trio.obj, geno.vec = geno.vec, dt.vec = c("sub7","sub8"), fd.indices = c(1,2,3,6), n = 1e2)
-
-## trios <- rep(list(NA),length(ped2trio.list))
-## names(trios) <- names(ped2trio.list)
-## trio.ids <- ped2trio.list[[names(trios)[1]]] # first trio
-## trios[paste0("trio",trio.ids$id)] <- new("Trio", id = trio.ids$id, spouse = trio.ids$spouse, offspring = list(trio.ids$offspring))
-
-## trios
+RVsharing:::GeneDropSim.fn( trio = trio.obj, geno.vec = geno.vec, dt.vec = c("sub7","sub8"), fd.indices = c(1,2,3,6), n = 1e4)
+RVsharing(test.ped.first.cousins)
 
 
-## trio.obj.list <- list()
-## k <- length(ped2trio.list)
+###################################################
+### code chunk number 6: secondcousins
+###################################################
+id <- paste0("sub", 1:12)
+fa.id <- c(NA,NA,NA,"sub1","sub1", NA,"sub3","sub5",NA,NA,"sub9","sub10")
+ma.id <- c(NA,NA,NA,"sub2","sub2", NA,"sub4","sub6",NA,NA,"sub7","sub8")
+test.ped.second.cousins <- pedigree(id = id, dadid = fa.id, momid = ma.id, sex = c(1,2,1,2,1,2,2,2,1,1,2,2))
+plot(test.ped.second.cousins)
 
-## for( i in 1:(k-1) ){
+trio.obj <- RVsharing:::ped2trio(test.ped.second.cousins)
 
-##     trio.obj.list[[i]] <- new("Trio", id = ped2trio.list[[i]]$id, spouse = ped2trio.list[[i]]$spouse, offspring = ped2trio.list[[i]]$offspring)
+geno.vec <- c(1,0,0,NA,NA,0,NA,NA,0,0,NA,NA)
+names(geno.vec) <- paste0("sub",1:12)
 
-## }
+RVsharing:::GeneDropSim.fn( trio = trio.obj, geno.vec = geno.vec, dt.vec = c("sub11","sub12")
+RVsharing(test.ped.second.cousins)
 
-## trio.obj <- new("Trio", id = ped2trio.list[[k]]$id, spouse = ped2trio.list[[k]]$spouse, offspring = trio.obj.list )
+
+
