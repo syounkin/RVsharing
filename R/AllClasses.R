3cf7afb36cdd932921d929b7269bdb74431b86be
abureau <abureau6@gmail.com>
2014-11-20 20:45:00 -0500

    Adding carriers vector to class RVsharingProb

--- R/AllClasses.R
+++ R/AllClasses.R
@@ -1,5 +1,5 @@
 setClass("Trio", representation = list( id = "character", spouse = "character", offspring = "list"))
-setClass("RVsharingProb", representation = list( pshare="numeric",iancestors="character",desfounders="list",id="character",dad.id="character",mom.id="character"))
+setClass("RVsharingProb", representation = list( pshare="numeric",iancestors="character",desfounders="list",id="character",dad.id="character",mom.id="character",carriers="character"))
 
 setClassUnion("TrioOrChar", c("Trio","character"))
 
