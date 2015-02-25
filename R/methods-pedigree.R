a5f4799b5ab17e953802af8d2635a28d5337b7f8
abureau <abureau6@gmail.com>
2015-02-25 17:04:55 -0500

    Removing carriers from methods when they are missing

--- R/methods-pedigree.R
+++ R/methods-pedigree.R
@@ -7,7 +7,7 @@ setMethod("RVsharing",  signature(data="pedigree", dad.id="missing",mom.id="miss
   
     RVsharing.fn( id = data$id, dad.id = fa.id, mom.id = ma.id, carriers=carriers )
 })
-setMethod("RVsharing",  signature(data="pedigree", dad.id="missing",mom.id="missing",carriers="missing"), function(data,carriers){
+setMethod("RVsharing",  signature(data="pedigree", dad.id="missing",mom.id="missing",carriers="missing"), function(data){
 
   fa.index <- ifelse(data$findex !=0, data$findex, NA)
   fa.id <- data$id[fa.index]
@@ -22,7 +22,7 @@ setMethod("RVsharing",  signature(data="character",dad.id="character",mom.id="ch
     mom.id = ifelse(mom.id=="0",NA,mom.id)
     RVsharing.fn( id = data, dad.id = dad.id, mom.id = mom.id, carriers=carriers )
 })
-setMethod("RVsharing",  signature(data="character",dad.id="character",mom.id="character",carriers="missing"), function(data, dad.id, mom.id, carriers){
+setMethod("RVsharing",  signature(data="character",dad.id="character",mom.id="character",carriers="missing"), function(data, dad.id, mom.id){
     # Converting 0s to NA  in dad.id and mom.id
     dad.id = ifelse(dad.id=="0",NA,dad.id)
     mom.id = ifelse(mom.id=="0",NA,mom.id)
@@ -34,7 +34,7 @@ setMethod("RVsharing",  signature(data="numeric",dad.id="numeric",mom.id="numeri
     mom.id = ifelse(mom.id==0,NA,mom.id)
     RVsharing.fn( id = data, dad.id = dad.id, mom.id = mom.id, carriers=carriers )
 })
-setMethod("RVsharing",  signature(data="numeric",dad.id="numeric",mom.id="numeric",carriers="missing"), function(data, dad.id, mom.id, carriers){
+setMethod("RVsharing",  signature(data="numeric",dad.id="numeric",mom.id="numeric",carriers="missing"), function(data, dad.id, mom.id){
     # Converting 0s to NA  in dad.id and mom.id
     dad.id = ifelse(dad.id==0,NA,dad.id)
     mom.id = ifelse(mom.id==0,NA,mom.id)
